081d88ece3c1d8d9a722a705b09859215c0b37e1
alipsidikp <alip.sidik.p@gmail.com>
2016-02-11 15:52:13 +0700

    Add Test Case
    Test Case SSH User-Password

--- test/live_test.go
+++ test/live_test.go
@@ -24,8 +24,20 @@ func TestSshKey(t *testing.T) {
 }
 
 func TestSshUsername(t *testing.T) {
-	t.Skip("Skip : Comment this line to do test")
-	//
-	// Do Test for login with username in here
-	//
+	// t.Skip("Skip : Comment this line to do test")
+	var SshClient SshSetting
+
+	SshClient.SSHAuthType = SSHAuthType_Password
+	SshClient.SSHHost = "192.168.56.101:22"
+	SshClient.SSHUser = "alip"
+	SshClient.SSHPassword = "Bismillah"
+
+	ps := []string{"sudo service mysql status"}
+	res, e := SshClient.RunCommandSsh(ps...)
+
+	if e != nil {
+		t.Errorf("Error, %s \n", e)
+	} else {
+		t.Logf("RUN, %s \n", res)
+	}
 }
